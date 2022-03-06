bc00d9a2851b7ccd9493f45ec66f174f78f79c5f
bondarenkoIlyaProfession <bondarenkoIlyaProfession@gmail.com>
2022-03-06 10:47:36 +0200

    created home components, changed container width

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+
+import portrait from "../../assets/images/portrait.png";
 
 export const Home = () => {
   return (
-    <>
+    <section className="home">
+      <div className="container">
+        <div className="home__text">
+          <h1 className="home__title">
+            Hi, I'm James Mirro & I'm a photographer.
+          </h1>
+          <div className="home__description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
+          </div>
+          <a href="#top" className="home__link">Read more</a>
+        </div>
 
-    </>
+        <div className="home__image">
+          <img src={portrait} alt="Portrait" />
+        </div>
+      </div>
+    </section>
   )
 }
